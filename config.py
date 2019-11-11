96d1324dfd54cd9145a7e4eb8d5808bc6e2ec7d6
Alonzo <arizagapepe@gmail.com>
2019-11-11 12:05:03 -0500

    Added option to chose dataset directory

--- config.py
+++ config.py
@@ -1,13 +1,24 @@
 import os
 
-INPUT_DATASET = 'datasets/original'
 
+fLocation = input('Manually input directory or hit space for default dataset directory: ')
+
+if fLocation == ' ':
+	INPUT_DATASET = 'datasets/original'
+
+else:
+	INPUT_DATASET = fLocation	
+
+print(INPUT_DATASET)
 BASE_PATH = 'datasets/idc'
 
 #creates and organizes paths
 TRAIN_PATH = os.path.sep.join([BASE_PATH, 'training'])
 VAL_PATH = os.path.sep.join([BASE_PATH, 'validation'])
 TEST_PATH = os.path.sep.join([BASE_PATH, 'testing'])
+
+#TODO
+#Ask for user input from terminal line for location to datasets
 
 
 #percent of data to use
